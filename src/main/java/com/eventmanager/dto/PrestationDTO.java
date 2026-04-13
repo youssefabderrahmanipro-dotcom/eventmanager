f1d6f5babb7fbd87eb45b8bbb4a01a98f313849a
Youssef <youssefabderrahmani.pro@gmail.com>
2026-04-13 16:40:54 +0100

    initial commit

--- src/main/java/com/eventmanager/dto/PrestationDTO.java
+++ src/main/java/com/eventmanager/dto/PrestationDTO.java
@@ -4,18 +4,14 @@ import lombok.Data;
 
 @Data
 public class PrestationDTO {
-
     private String id;
-
     private String nom;
-
     private String description;
-
-    private Double prix;
-
-    private String categorie;
-
+    private double prix;
     private String statut;
+
+    private Long categorieId;
+    private String categorieNom;
 
     private String proprietaireId;
 }
