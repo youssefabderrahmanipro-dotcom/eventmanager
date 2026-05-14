84355eddeadfc3b696daa09f56a1948fad9e02b6
Youssef <youssefabderrahmani.pro@gmail.com>
2026-05-14 11:15:38 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/eventmanager/service/CategorieService.java
    #	target/classes/com/eventmanager/service/MapperService.class

---
+++ src/main/java/com/eventmanager/dto/UpdateUtilisateurDTO.java
@@ -0,0 +1,15 @@
+package com.eventmanager.dto;
+
+public class UpdateUtilisateurDTO {
+    private String nom;
+    private String email;
+    private String telephone;
+
+    // Getters & Setters
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getTelephone() { return telephone; }
+    public void setTelephone(String telephone) { this.telephone = telephone; }
+}
